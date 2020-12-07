78328029896e15049f2ad4d5fb8ede16f37a0c93
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2020-12-07 19:13:39 +0100

    Created search form function

--- src/app.js
+++ src/app.js
@@ -28,20 +28,29 @@ let month = months[currentDate.getMonth()];
 
 let date = currentDate.getDate();
 
-let fullDate = document.querySelector("#date");
-fullDate.innerHTML = `${day}, ${month} ${date}`;
+document.querySelector("#date").innerHTML = `${day}, ${month} ${date}`;
 
-function displayTemperature(response){
-console.log(response.data);
-document.querySelector("#city").innerHTML = response.data.name;
-document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
-document.querySelector("#description").innerHTML = response.data.weather[0].description;
-document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
-document.querySelector("#humidity-data").innerHTML = response.data.main.humidity;
-document.querySelector("#wind-data").innerHTML = Math.round(response.data.wind.speed);
+function search(city){
+  let apiKey = "027401657e14d2712c8487adaadbd48b";
+  let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-let apiKey = "027401657e14d2712c8487adaadbd48b";
-let city = "Ayent";
-let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function handlesubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function displayTemperature(response){
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
+  document.querySelector("#humidity-data").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-data").innerHTML = Math.round(response.data.wind.speed);
+}
+
+search("Cork");
+
+document.querySelector("#search-form").addEventListener("submit", handlesubmit);
