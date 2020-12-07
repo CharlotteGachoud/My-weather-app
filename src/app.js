631fdd032ad637ddba2f969a77b8a9bff327b480
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2020-12-07 22:28:10 +0100

    Updated unit converstion

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ function displayCelsiusTemperature(event){
 
 let celsiusTemperature = null;
 
-let fahrenheitLink = document.querySelector("#farenheit-link")
+let fahrenheitLink = document.querySelector("#fahrenheit-link")
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
