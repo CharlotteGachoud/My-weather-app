d8f16838f5e3dde46be1fc3bc63c4ec2666e4f5c
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2020-12-09 21:31:54 +0100

    Changed App design and image

--- src/app.js
+++ src/app.js
@@ -148,5 +148,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
+ 
 search("Cork");
